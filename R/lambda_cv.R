0b09d20af6ae9f2de258c5d18e81c55b57a466d4
Adina <roskwabl.3@mail.de>
2026-02-21 23:53:02 +0100

    Weitergearbeitet an der Funktion, noch nicht fertig

--- R/lambda_cv.R
+++ R/lambda_cv.R
@@ -5,7 +5,7 @@
 #' @param X Numeric matrix of predictors (n x p), should be standardized.
 #' @param y Numeric response vector of length n, should be centered.
 #' @param M Positive natural number (Number of Unterteilungen von den Beobachtungen???)
-#' @param e 1 or 2 for LASSO or Ridge estimation ?? (wie bei mapply)
+#' @param method character vector ("lasso" or "ridge")
 #'
 #' @returns A list with:
 #' \describe{
@@ -26,10 +26,11 @@
 #' std <- standardize_data(X,y)
 #' lambda_CV(std$X,std$y,e=1)
 #'
-lambda_CV <- function(X,y,M = 5, e){
+lambda_CV <- function(X, y, M = 5, method){
   M <- as.integer(M)
   if(M <= 0) stop("M musst be a positive number")
-  if(!e %in% c(1,2)) stop("e must be 1 (lasso) or 2 (ridge)")
+  stopifnot("method musst bei lasso or ridge" = (method %in% c("lasso","ridge")))
+  #if(!e %in% c(1,2)) stop("e must be 1 (lasso) or 2 (ridge)")
   #beim Rest muss ich mal gucken, wie die anderen das jetzt mit dem Standartisieren gelöst haben
 
 
@@ -38,7 +39,20 @@ lambda_CV <- function(X,y,M = 5, e){
   #eher bei bsp, oder?  set.seed(1) # statt mit 1 evtl. mit seed arbeiten (wenn NULL, dann "echtere" Zufallszahl)
   fold_index <- sample(rep(1:M, length.out = n), #length out führt dazu, dass immer n zugordnet werden könnnen, auch wenn M gar kein Teiler von n ist (dann die vorderen Indexmengen etwas größer, d.h. nicht exkat gleich groß, reicht aber)
                        size = n, replace = FALSE)
+  #if(method == "lasso") lambda_seq <- lambda_sequence(X,y)
+  #else lambda_seq <- lambda_sequence_ridge(X,y)
+
+  std <- standardize_data(X,y)
+  X <- std$X
+  y <- std$y
   lambda_seq <- lambda_sequence(X,y)
+  #if(method == "lasso"){
+  #  std <- standardize_data(X, y)
+  ##  lambda_seq <- lambda_sequence(std$X, std$y)
+  #} else if(method == "ridge"){
+  #  lambda_seq <- lambda_sequence_ridge(X)
+  #}
+
   cv <- numeric(length(lambda_seq))
   for (l in seq_along(lambda_seq)){ #geht das ? Kein integer Vektor?
     res <- 0
@@ -47,23 +61,35 @@ lambda_CV <- function(X,y,M = 5, e){
       X_test <- X[fold_index == m, , drop =  FALSE]
       y_train <- y[fold_index != m]
       y_test <- y[fold_index == m]
-      if(e==1){
+      if(method == "lasso"){
+       # std <- standardize_data(X_train, y_train)
+
         beta <- lasso_cd(X_train, y_train, lambda_seq[l])$beta #ist das jetzt ein Vektor?
+        intercept <- 0
       }
-      if(e==2){
-        beta <- ridge_cd(X_train, y_train, lambda_seq[l])$beta #heißt die Funktion so?
+
+      if(method == "ridge"){
+       # ridge_data <- ridge(X_train, y_train, lambda_seq[l]) #Name?
+      #  beta <- ridge_data$coefficients
+       # intercept <- ridge_data$intercept
+        beta <- ridge_core(X_train, y_train, lambda_seq[l]) #!irgendwie Verschiebung um 10er Stelle in Beispielen (vllt. um p?)
+        intercept <- 0
       }
       #y_pred <-  X_test %*% beta #Skalarprodukt Name der Variable?
+
+      #res <- res + sum((y_test - y_pred)^2)
+      #y_pred <- intercept + X_test %*% beta
 
       #res <- res + sum((y_test - y_pred)^2)
 
       #Alternativ (nahe an der Definition)
       res <- res + sum(sapply(which(fold_index == m), function(i){
-        (y[i] - beta %*% X[i,])^2
+        (y[i] - (intercept + sum(beta*X[i,])))^2
+       # (y[i] - (intercept + beta %*% X[i,]))^2
        #(y[i] - (beta[1] + sum(beta[-1]*X[i,])))^2 #das ist L(Yi,f schlange von Xi)
         #etwas Verwirrung, aber bei Standartisierung ist wohl beta0 =0, d.h. die erste Komponente wird meistens weggelassen (sogenanntes intercept)
       }))
-    cv[l] <- 1/n*res
+    cv[l] <- res/n
     }
   }
   return(list(
@@ -72,4 +98,3 @@ lambda_CV <- function(X,y,M = 5, e){
     lambda_seq = lambda_seq
   ))
 }
-# statt e evtl. method und mit character statt Zahl?
