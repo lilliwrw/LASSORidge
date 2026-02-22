840e19aace91e8ad724eb1b8bc806e7834a144a3
Adina <roskwabl.3@mail.de>
2026-02-22 17:17:41 +0100

    erstellen plot_cv und arbeiten an lambda_cv

--- R/lambda_cv.R
+++ R/lambda_cv.R
@@ -23,38 +23,31 @@
 #' X <- matrix(rnorm(n*p), n, p)
 #' beta_true <- c(3, -2, 1.5, rep(0, p-3))
 #' y <- X %*% beta_true + rnorm(n)
-#' std <- standardize_data(X,y)
-#' lambda_CV(std$X,std$y,e=1)
+#' lambda_CV(X,y,method="lasso")
+#' lambda_CV(X,y,method="ridge")
 #'
 lambda_CV <- function(X, y, M = 5, method){
   M <- as.integer(M)
   if(M <= 0) stop("M musst be a positive number")
   stopifnot("method musst bei lasso or ridge" = (method %in% c("lasso","ridge")))
-  #if(!e %in% c(1,2)) stop("e must be 1 (lasso) or 2 (ridge)")
-  #beim Rest muss ich mal gucken, wie die anderen das jetzt mit dem Standartisieren gelöst haben
-
 
   n <- nrow(X)
-  #prüfen X,Y standartisiert richtige Grööße etc.
-  #eher bei bsp, oder?  set.seed(1) # statt mit 1 evtl. mit seed arbeiten (wenn NULL, dann "echtere" Zufallszahl)
   fold_index <- sample(rep(1:M, length.out = n), #length out führt dazu, dass immer n zugordnet werden könnnen, auch wenn M gar kein Teiler von n ist (dann die vorderen Indexmengen etwas größer, d.h. nicht exkat gleich groß, reicht aber)
                        size = n, replace = FALSE)
-  #if(method == "lasso") lambda_seq <- lambda_sequence(X,y)
-  #else lambda_seq <- lambda_sequence_ridge(X,y)
 
   std <- standardize_data(X,y)
   X <- std$X
   y <- std$y
-  lambda_seq <- lambda_sequence(X,y)
-  #if(method == "lasso"){
+  #lambda_seq <- lambda_sequence(X,y) #! vllt. doch andere lambda Sequenz für ridge
+  if(method == "lasso"){
   #  std <- standardize_data(X, y)
-  ##  lambda_seq <- lambda_sequence(std$X, std$y)
-  #} else if(method == "ridge"){
-  #  lambda_seq <- lambda_sequence_ridge(X)
-  #}
+    lambda_seq <- lambda_sequence(std$X, std$y)
+  } else if(method == "ridge"){
+    lambda_seq <- lambda_sequence_ridge(X,y)
+  }
 
   cv <- numeric(length(lambda_seq))
-  for (l in seq_along(lambda_seq)){ #geht das ? Kein integer Vektor?
+  for (l in seq_along(lambda_seq)){
     res <- 0
     for (m in 1:M){
       X_train <- X[fold_index != m, , drop = FALSE]
@@ -64,7 +57,7 @@ lambda_CV <- function(X, y, M = 5, method){
       if(method == "lasso"){
        # std <- standardize_data(X_train, y_train)
 
-        beta <- lasso_cd(X_train, y_train, lambda_seq[l])$beta #ist das jetzt ein Vektor?
+        beta <- lasso_cd(X_train, y_train, lambda_seq[l])$beta
         intercept <- 0
       }
 
@@ -73,24 +66,21 @@ lambda_CV <- function(X, y, M = 5, method){
       #  beta <- ridge_data$coefficients
        # intercept <- ridge_data$intercept
         beta <- ridge_core(X_train, y_train, lambda_seq[l]) #!irgendwie Verschiebung um 10er Stelle in Beispielen (vllt. um p?)
-        intercept <- 0
+        intercept <- 0 #stimmt das hier? ist es standartisiert?
       }
-      #y_pred <-  X_test %*% beta #Skalarprodukt Name der Variable?
+      y_pred <- intercept + X_test %*% beta
 
-      #res <- res + sum((y_test - y_pred)^2)
-      #y_pred <- intercept + X_test %*% beta
-
-      #res <- res + sum((y_test - y_pred)^2)
+      res <- res + sum((y_test - y_pred)^2)
 
       #Alternativ (nahe an der Definition)
-      res <- res + sum(sapply(which(fold_index == m), function(i){
-        (y[i] - (intercept + sum(beta*X[i,])))^2
+      #res <- res + sum(sapply(which(fold_index == m), function(i){
+      #  (y[i] - (intercept + sum(beta*X[i,])))^2
        # (y[i] - (intercept + beta %*% X[i,]))^2
        #(y[i] - (beta[1] + sum(beta[-1]*X[i,])))^2 #das ist L(Yi,f schlange von Xi)
         #etwas Verwirrung, aber bei Standartisierung ist wohl beta0 =0, d.h. die erste Komponente wird meistens weggelassen (sogenanntes intercept)
-      }))
-    cv[l] <- res/n
+      #}))
     }
+    cv[l] <- res/n
   }
   return(list(
     lambda_opt = lambda_seq[which.min(cv)],
