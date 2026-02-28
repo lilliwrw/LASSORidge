fab57406f7b5097d40ec03e9527e166f5b33e392
juliwenn <mails@elwenn.de>
2026-02-28 12:44:11 +0100

    Umbenennen von `coef_lasso.R`in `coef.lasso_model.R`und `predict_lasso.R`in `predict.Lasso_model.R`

--- tests/testthat/test-coef.lasso_model.R
+++ tests/testthat/test-coef.lasso_model.R
@@ -1,14 +1,14 @@
-test_that("coef_lasso extracts correct column", {
+test_that("coef() for lasso_model extracts correct column", {
   set.seed(1)
   X <- matrix(rnorm(20*5), 20, 5)
   y <- rnorm(20)
 
   fit <- lasso(X, y, n_lambda = 4)
 
-  coefs_last <- coef_lasso(fit)
+  coefs_last <- coef(fit)
   expect_equal(dim(coefs_last), c(5, 1))
 
-  coefs_second <- coef_lasso(fit, lambda_index = 2)
+  coefs_second <- coef(fit, lambda_index = 2)
   expect_equal(dim(coefs_second), c(5, 1))
 
   expect_true(is.numeric(coefs_last))
--- tests/testthat/test-predict.lasso_model.R
+++ tests/testthat/test-predict.lasso_model.R
@@ -1,4 +1,4 @@
-test_that("predict_lasso returns correct length", {
+test_that("predict() for lasso_model returns correct length", {
   set.seed(1)
   X <- matrix(rnorm(20*5), 20, 5)
   y <- rnorm(20)
@@ -6,11 +6,11 @@ test_that("predict_lasso returns correct length", {
   fit <- lasso(X, y, n_lambda = 5)
 
   X_new <- matrix(rnorm(10*5), 10, 5)
-  pred <- predict_lasso(fit, X_new)
+  pred <- predict(fit, X_new)
 
   expect_equal(length(pred), nrow(X_new))
   expect_true(is.numeric(pred))
 
-  pred2 <- predict_lasso(fit, X_new, lambda_index = 2)
+  pred2 <- predict(fit, X_new, lambda_index = 2)
   expect_equal(length(pred2), nrow(X_new))
 })
